8c942a9e82043191acbb30cf1c48f25b0115ccf3
Falcon <lucas.falcon@etu.univ-lyon1.fr>
2017-01-24 17:09:40 +0100

    player controller script & player prefab

---
+++ Neon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        //rotation
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        //translation
+        Vector3 move_direction = new Vector3(0, 0, 0);
+
+        if(Input.GetKey(KeyCode.Z))
+        {
+            move_direction.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            move_direction.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            move_direction.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            move_direction.x += 1;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            move_direction.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            move_direction.y -= 1;
+        }
+        transform.Translate(move_direction.normalized * Time.deltaTime * speed);
+
+
+    }
+}

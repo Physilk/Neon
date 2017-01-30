85ef94eea87406402a009ed8d1330dcab94c6e31
Falcon <lucas.falcon@etu.univ-lyon1.fr>
2017-01-30 17:49:11 +0100

    player hit

--- Neon/Assets/Scripts/PlayerController.cs
+++ Neon/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,29 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
-    public float speed;
+    public float speed = 10.0f;
     public float speedH = 2.0f;
     public float speedV = 2.0f;
-    public float dashSpeed = 5.0f;
-    public float dashDuration = 1.0f;
-    public float shootDuration = 0.25f;
+    public float dashSpeed = 40.0f;
+    public float dashDuration = 0.2f;
+    public float shootDuration = 0.02f;
     public float maxLaserDistance = 2000.0f;
+    public float hitDuration = 0.1f;
+    public float hitSpeed = 100.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
     private bool dashing = false;
     private float dashStartTime;
-    private Vector3 dashDirection;
+    private Vector3 dashDirectionWorldSpace;
 
     private bool shooting = false;
     private float shootingStartTime;
+
+    private bool affectedByHit = false;
+    private float hitStartTime;
+    private Vector3 hitDirectionWorldSpace;
 
     private GameObject laser;
 
@@ -35,33 +41,39 @@ public class PlayerController : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
         //if (!isLocalPlayer)
-         //   return;
+        //   return;
 
-        Move();
-        Shoot();
+        Hit();
+        if (!affectedByHit)
+        {
+            Move();
+            Shoot();
+        }
         if (Input.GetKey(KeyCode.Escape))
             Screen.lockCursor = false;
     }
 
     void Move()
     {
-        if(dashing)
-        {
-            if(Time.time - dashStartTime > dashDuration)
-            {
-                dashing = false;
-            } else
-            {
-                transform.Translate(dashDirection.normalized * Time.deltaTime * dashSpeed);
-                return;
-            }
-
-        }
         //rotation
         yaw += speedH * Input.GetAxis("Mouse X");
         pitch -= speedV * Input.GetAxis("Mouse Y");
 
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        if (dashing)
+        {
+            if (Time.time - dashStartTime > dashDuration)
+            {
+                dashing = false;
+            }
+            else
+            {
+                transform.Translate(dashDirectionWorldSpace.normalized * Time.deltaTime * dashSpeed, Space.World);
+                return;
+            }
+
+        }
 
         //translation
         Vector3 move_direction = new Vector3(0, 0, 0);
@@ -100,8 +112,8 @@ public class PlayerController : NetworkBehaviour {
         {
             dashing = true;
             dashStartTime = Time.time;
-            dashDirection = move_direction;
-            transform.Translate(dashDirection.normalized * Time.deltaTime * dashSpeed);
+            dashDirectionWorldSpace = transform.TransformDirection(move_direction);//Transform.Trans(move_direction);
+            transform.Translate(dashDirectionWorldSpace.normalized * Time.deltaTime * dashSpeed, Space.World);
         } else transform.Translate(move_direction.normalized * Time.deltaTime * speed);
 
 
@@ -123,9 +135,15 @@ public class PlayerController : NetworkBehaviour {
 
             if (Physics.Raycast(transform.position, fwd, out hit, dist)) {
                 dist = hit.distance;
+                if (hit.transform != null)
+                    if(hit.transform.gameObject.tag == "Player")
+                    {
+                        hit.transform.gameObject.GetComponent<PlayerController>().SetHit(-fwd);
+                    }
             }
             laser.transform.localScale = new Vector3(1, 1, dist);
             laser.GetComponent<LineRenderer>().enabled = true;
+            
 
         } if (shooting)
         {
@@ -135,5 +153,26 @@ public class PlayerController : NetworkBehaviour {
                 laser.GetComponent<LineRenderer>().enabled = false;
             }
         }
+    }
+
+    void SetHit(Vector3 hitDirection)
+    {
+        if (affectedByHit)
+            return;
+        else affectedByHit = true;
+        hitDirectionWorldSpace = hitDirection;
+        hitStartTime = Time.time;
+    }
+
+    void Hit()
+    {
+        if (!affectedByHit)
+            return;
+        if(Time.time - hitStartTime > hitDuration)
+        {
+            affectedByHit = false;
+            return;
+        }
+        transform.Translate(hitDirectionWorldSpace.normalized * Time.deltaTime * hitSpeed, Space.World);
     }
 }

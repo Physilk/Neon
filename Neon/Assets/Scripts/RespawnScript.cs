cab3477adddf9b79344937811c287ca085c44df8
Lucas Falcon <lucas.falcon@etu.univ-lyon1.fr>
2017-02-05 21:11:58 +0100

    scene solo et respawn script

---
+++ Neon/Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnScript : MonoBehaviour {
+
+    public GameObject player;
+    private Transform transform_spawn;
+
+	// Use this for initialization
+	void Start () {
+        transform_spawn = player.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
